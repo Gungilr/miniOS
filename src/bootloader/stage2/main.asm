f259e7f8d96377ae71c1eed113e5577d349f5525
Rhystic <4nimefan@gmail.com>
2025-03-03 04:29:19 -0500

    the bug was a pointer

--- src/bootloader/stage2/main.asm
+++ src/bootloader/stage2/main.asm
@@ -7,13 +7,14 @@ global entry
 
 entry:
     cli
+    ; setup stack
     mov ax, ds
     mov ss, ax
     mov sp, 0
     mov bp, sp
-    sti 
+    sti
 
-    ; expect boot drive in dl, send it as an argument to cstart function
+    ; expect boot drive in dl, send it as argument to cstart function
     xor dh, dh
     push dx
     call _cstart_
--- src/bootloader/stage2/x86.h
+++ src/bootloader/stage2/x86.h
@@ -1,3 +1,5 @@
 #include "stdint.h"
 
+void _cdecl x86_div64_32(uint64_t divdend, uint32_t divsor, uint64_t* quotientOut, uint32_t* remainderOut);
+
 void _cdecl x86_Video_WriteCharTeletype(char c, uint8_t page);
