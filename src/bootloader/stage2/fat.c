53b57d82bf70dbfce1c7b7757747a8ba9c520f06
Rhystic <4nimefan@gmail.com>
2025-03-20 01:49:04 -0400

    WIP

---
+++ src/bootloader/stage2/fat.c
@@ -0,0 +1,201 @@
+#include "fat.h"
+#include "stdio.h"
+#include "memdefs.h"
+
+#define SECTOR_SIZE     512
+
+#pragma pack(push, 1)
+typedef struct 
+{
+    uint8_t BootJumpInstruction[3];
+    uint8_t OemIdentifier[8];
+    uint16_t BytesPerSector;
+    uint8_t SectorsPerCluster;
+    uint16_t ReservedSectors;
+    uint8_t FatCount;
+    uint16_t DirEntryCount;
+    uint16_t TotalSectors;
+    uint8_t MediaDescriptorType;
+    uint16_t SectorsPerFat;
+    uint16_t SectorsPerTrack;
+    uint16_t Heads;
+    uint32_t HiddenSectors;
+    uint32_t LargeSectorCount;
+
+    // extended boot record
+    uint8_t DriveNumber;
+    uint8_t _Reserved;
+    uint8_t Signature;
+    uint32_t VolumeId;          // serial number, value doesn't matter
+    uint8_t VolumeLabel[11];    // 11 bytes, padded with spaces
+    uint8_t SystemId[8];
+
+    // ... we don't care about code ...
+
+} FAT_BootSector;
+#pragma pack(pop)
+
+typedef struct
+{
+    union
+    {
+        FAT_BootSector BootSector;
+        uint8_t BootSectorBytes[SECTOR_SIZE];
+    } BS;
+
+} FAT_Data;
+
+static FAT_Data far* g_Data;
+
+uint8_t* g_Fat = NULL;
+DirectoryEntry* g_RootDirectory = NULL;
+uint32_t g_RootDirectoryEnd;
+
+bool FAT_ReadBootSector(DISK* disk)
+{
+    return DISK_ReadSectors(disk, 0, 1, g_Data->BS.BootSectorBytes);
+}
+
+
+bool FAT_Initalize(DISK* disk) 
+{
+    g_Data = (FAT_Data far*)MEMORY_FAT_ADDR;
+    
+    if(!FAT_ReadBootSector(disk))
+    {
+        printf("FAT: read boot sector failure\r\n");
+    }
+}
+
+
+bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut)
+{
+    bool ok = true;
+    ok = ok && (fseek(disk, lba * g_BootSector.BytesPerSector, SEEK_SET) == 0);
+    ok = ok && (fread(bufferOut, g_BootSector.BytesPerSector, count, disk) == count);
+    return ok;
+}
+
+bool readFAT(FILE* disk)
+{
+    g_Fat = (uint8_t*) malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector);
+    return readSectors(disk, g_BootSector.ReservedSectors, g_BootSector.SectorsPerFat, g_Fat);
+}
+
+bool readRootDirectory(FILE* disk)
+{
+    uint32_t lba = g_BootSector.ReservedSectors + g_BootSector.SectorsPerFat * g_BootSector.FatCount;
+    uint32_t size = sizeof(DirectoryEntry) * g_BootSector.DirEntryCount;
+    uint32_t sectors = (size / g_BootSector.BytesPerSector);
+    if (size % g_BootSector.BytesPerSector > 0)
+        sectors++;
+
+    g_RootDirectoryEnd = lba + sectors;
+    g_RootDirectory = (DirectoryEntry*) malloc(sectors * g_BootSector.BytesPerSector);
+    return readSectors(disk, lba, sectors, g_RootDirectory);
+}
+
+
+DirectoryEntry* findFile(const char* name)
+{
+    for (uint32_t i = 0; i < g_BootSector.DirEntryCount; i++)
+    {
+        if (memcmp(name, g_RootDirectory[i].Name, 11) == 0)
+            return &g_RootDirectory[i];
+    }
+
+    return NULL;
+}
+
+bool readFile(DirectoryEntry* FileEntry, FILE* disk, uint8_t* outputBuffer) 
+{
+    bool ok = true;
+    uint16_t currentCluster = FileEntry->FirstClusterLow;
+    uint32_t bytesRemaining = FileEntry->Size;
+
+    do {
+        uint32_t lba = g_RootDirectoryEnd + (currentCluster - 2) * g_BootSector.SectorsPerCluster;
+        fprintf(stdout, "%d\n", &lba);
+
+        // Read only what's necessary
+        uint32_t bytesToRead = g_BootSector.SectorsPerCluster * g_BootSector.BytesPerSector;
+        if (bytesToRead > bytesRemaining)  // Prevent over-read
+            bytesToRead = bytesRemaining;
+
+        //ok = ok && readSectors(disk, lba, g_BootSector.SectorsPerCluster, outputBuffer); // this line is breaking
+        ok = ok && readSectors(disk, lba, (bytesToRead + g_BootSector.BytesPerSector - 1) / g_BootSector.BytesPerSector, outputBuffer);
+        outputBuffer += bytesToRead;
+        bytesRemaining -= bytesToRead;
+        
+
+        uint32_t fatIndex = currentCluster * 3 / 2;
+        if (currentCluster % 2  == 0) {
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) && 0xFFF;
+        } else {
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) >> 4;
+        }
+    } while(ok && currentCluster < 0xFF8);
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        printf("Syntax: %s <disk image> <file name>\n");
+        return -1;
+    }
+
+    FILE* disk = fopen(argv[1], "rb");
+    
+    if(!disk) {
+        fprintf(stderr, "Cannot open disk image %s!\n", argv[1]);
+        return -1;
+    }
+
+    if(!readBootSector(disk)) {
+        fprintf(stderr, "Could not read boot sector\n", argv[1]);
+        return -2;
+    }
+
+    if(!readFAT(disk)) {
+        fprintf(stderr, "Could not read FAT!\n");
+        free(g_Fat);
+        return -3;
+    }
+
+    if(!readRootDirectory(disk)) {
+        fprintf(stderr, "Could not read Root Directory!\n");
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -4;
+    }
+
+    DirectoryEntry* fileEntry = findFile(argv[2]);
+    if (!fileEntry) {
+        fprintf(stderr, "Could not find file %s!\n", argv[2]);
+        free(g_Fat);
+        free(g_RootDirectory);
+        return -5;
+    }
+
+    uint8_t* buffer = (uint8_t*) malloc(fileEntry->Size + g_BootSector.BytesPerSector);
+
+    
+    if (!readFile(fileEntry, disk, buffer)) {
+        fprintf(stderr, "Could not read file", argv[2]);
+        free(g_Fat);
+        free(g_RootDirectory);
+        free(buffer);
+        return -5;
+    }
+
+    for (size_t i = 0; i < fileEntry->Size; i++) {
+        if (isprint(buffer[i]))
+            fputc(buffer[i], stdout);
+        else
+            printf("<%02x>", buffer);
+    }
+    printf("\n");
+
+    free(g_Fat);
+    free(g_RootDirectory);
+    return 0; 
+}
--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -11,24 +11,24 @@ void _cdecl cstart_(uint16_t bootDrive)
 
     printf("Initializing disk...\n");
     if (!DISK_Initialize(&disk, driveNumber)) {
-        printf("Failed to initialize disk %d\n", driveNumber);
+        printf("Failed to initialize disk %d\r\n", driveNumber);
         return;
     }
 
-    printf("Disk initialized: Cylinders: %d, Heads: %d, Sectors: %d\n", disk.cylinders, disk.heads, disk.sectors);
+    printf("Disk initialized: Cylinders: %d, Heads: %d, Sectors: %d\r\n", disk.cylinders, disk.heads, disk.sectors);
 
     // Test LBA to CHS conversion
     uint32_t testLBA = 0;  // Example LBA sector
     uint16_t cylinder, sector, head;
 
     DISK_LBA2CHS(&disk, testLBA, &cylinder, &sector, &head);
-    printf("LBA %d -> Cylinder: %d, Head: %d, Sector: %d\n", testLBA, cylinder, head, sector);
+    printf("LBA %d -> Cylinder: %d, Head: %d, Sector: %d\r\n", testLBA, cylinder, head, sector);
 
     // Test reading a sector
     uint8_t buffer[512];  // One sector buffer
     DISK_ReadSectors(&disk, 19, 1, *g_data);
 
-    printf("%s\n", g_data);
+    printf("%s\r\n", g_data);
 
     return;
 
